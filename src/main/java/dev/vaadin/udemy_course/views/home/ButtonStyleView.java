7e5b8892069f4eef9df4fcaa3c7ea23f51fd651b
Stefan Wegner <stefan.wegner.1@gmx.de>
2025-08-09 17:12:16 +0200

    -button component demonstration

---
+++ src/main/java/dev/vaadin/udemy_course/views/home/ButtonStyleView.java
@@ -0,0 +1,46 @@
+package dev.vaadin.udemy_course.views.home;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.icon.SvgIcon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.streams.DownloadHandler;
+import com.vaadin.flow.server.streams.FileDownloadHandler;
+
+import java.nio.file.Paths;
+
+@Route("button-style")
+public class ButtonStyleView extends VerticalLayout
+{
+    public ButtonStyleView()
+    {
+        var btnPrimary = new Button("Primary + Success");
+        btnPrimary.addThemeVariants(ButtonVariant.LUMO_PRIMARY, ButtonVariant.LUMO_SUCCESS);
+
+        var btnSuccess = new Button("Success");
+        btnSuccess.addThemeVariants(ButtonVariant.LUMO_SUCCESS);
+
+        var btnTertiary = new Button("Tertiary + Success");
+        btnTertiary.addThemeVariants(ButtonVariant.LUMO_TERTIARY, ButtonVariant.LUMO_SUCCESS);
+
+        var btnIcon = new Button(VaadinIcon.ADOBE_FLASH.create());
+        btnIcon.setText("Icon");
+        btnIcon.addThemeVariants(ButtonVariant.LUMO_ERROR);
+
+        DownloadHandler resource = new FileDownloadHandler(Paths.get("src/main/resources/static/icons/mylogo.svg").toFile());
+        SvgIcon customIcon = new SvgIcon(resource);
+
+        var btnCustomIcon = new Button(customIcon);
+        btnCustomIcon.setText("My Custom Icon");
+        btnCustomIcon.setIconAfterText(true);
+        btnCustomIcon.addThemeVariants(ButtonVariant.LUMO_PRIMARY, ButtonVariant.LUMO_WARNING);
+
+        var btnStyling = new Button("Background Styling");
+        btnStyling.getStyle().set("background","green");
+        btnStyling.getStyle().set("color","white");
+
+        add(btnPrimary, btnSuccess, btnTertiary, btnIcon, btnCustomIcon, btnStyling);
+    }
+}
---
+++ src/main/java/dev/vaadin/udemy_course/views/home/ButtonView.java
@@ -0,0 +1,52 @@
+package dev.vaadin.udemy_course.views.home;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("button")
+public class ButtonView extends VerticalLayout
+{
+    public ButtonView()
+    {
+        var btnClick = new Button("Show");
+        btnClick.addClickListener(buttonClickEvent -> Notification.show("Hello World"));
+
+        var btnDoubleClick = new Button("Double");
+        btnDoubleClick.addDoubleClickListener(buttonClickEvent -> Notification.show("Double Clicked"));
+
+        var btnSingleClick = new Button("Single");
+        btnSingleClick.addSingleClickListener(buttonClickEvent -> Notification.show("Single Clicked"));
+
+        var btnShortcut = new Button("Shortcut ENTER");
+        btnShortcut.addClickShortcut(Key.ENTER);
+        btnShortcut.addSingleClickListener(buttonClickEvent -> Notification.show("Shortcut Clicked"));
+
+        var btnDisabled = new Button("Disabled on Click");
+        btnDisabled.setDisableOnClick(true);
+        btnDisabled.addClickListener(buttonClickEvent ->
+        {
+            Notification.show("Button Disabled on Click");
+            try
+            {
+                Thread.sleep(2500);
+            } catch (InterruptedException e)
+            {
+                Notification.show("interrupted, try again later");
+            }
+            btnDisabled.setEnabled(true);
+            Notification.show("Button Re-enabled");
+        });
+
+        var btnTooltip = new Button("Shortcut ENTER");
+        btnTooltip.setText("This is a Tooltip");
+        btnTooltip.addClickListener(buttonClickEvent -> Notification.show("Clicked"));
+
+        var btnFocus = new Button("Focus");
+        btnFocus.addFocusListener(event -> Notification.show("Focused"));
+
+        add(btnClick, btnDoubleClick, btnSingleClick, btnShortcut, btnDisabled, btnTooltip, btnFocus);
+    }
+}
