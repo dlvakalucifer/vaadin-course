44c4ee14b14bbff1e39f78f2bf67cb8a50086113
Stefan Wegner <stefan.wegner.1@gmx.de>
2025-08-09 21:56:03 +0200

    - binder example PT.1

--- src/main/java/dev/vaadin/udemy_course/model/Person.java
+++ src/main/java/dev/vaadin/udemy_course/model/Person.java
@@ -2,4 +2,71 @@ package dev.vaadin.udemy_course.model;
 
 public class Person
 {
+    private String name;
+    private String mobile;
+    private int age;
+    private String address;
+    private String email;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName( String name )
+    {
+        this.name = name;
+    }
+
+    public String getMobile()
+    {
+        return mobile;
+    }
+
+    public void setMobile( String mobile )
+    {
+        this.mobile = mobile;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public void setAge( int age )
+    {
+        this.age = age;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress( String address )
+    {
+        this.address = address;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail( String email )
+    {
+        this.email = email;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", mobile='" + mobile + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
--- src/main/java/dev/vaadin/udemy_course/view/binder/PersonView.java
+++ src/main/java/dev/vaadin/udemy_course/view/binder/PersonView.java
@@ -1,19 +1,86 @@
 package dev.vaadin.udemy_course.view.binder;
 
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.data.converter.StringToIntegerConverter;
 import com.vaadin.flow.router.Route;
+import dev.vaadin.udemy_course.model.Person;
 
+// http://localhost:8080/person-form
 @Route("person-form")
 public class PersonView extends VerticalLayout
 {
+    private Person person;
+
     public PersonView()
     {
+        person = loadDefaultPerson();
+        loadDefaultPerson();
         setSizeFull();
         createForm();
+    }
+
+    private Person loadDefaultPerson()
+    {
+        Person person = new Person();
+        person.setName("John");
+        person.setMobile("01712182927");
+        person.setAddress("Some Fancy Place");
+        person.setAge(30);
+        person.setEmail("John@Domain.com");
+
+        return person;
     }
 
     private void createForm()
     {
 
+        var formLayout = new FormLayout();
+        formLayout.setWidth("100%");
+        formLayout.setHeight("100%");
+
+        var tfName = new TextField("Name");
+        var tfMobile = new TextField("Mobile");
+        var tfAddress = new TextField("Address");
+        var tfEmail = new TextField("Email");
+        var tfAge = new TextField("Age");
+
+        var horizontalLayout = new HorizontalLayout();
+
+        var btSave = new Button("Save");
+        var btCancel = new Button("Cancel");
+
+        var binder = new Binder<>(Person.class);
+        binder.forField(tfName).bind(Person::getName, Person::setName);
+        binder.forField(tfMobile).bind(Person::getMobile, Person::setMobile);
+        binder.forField(tfAddress).bind(Person::getAddress, Person::setAddress);
+        binder.forField(tfAge).withConverter(new StringToIntegerConverter("not an age")).bind(Person::getAge, Person::setAge);
+        binder.forField(tfEmail).bind(Person::getEmail, Person::setEmail);
+
+
+        horizontalLayout.add(btSave, btCancel);
+
+        formLayout.add(tfName, tfMobile, tfAge, tfAddress, tfEmail, horizontalLayout);
+
+        add(formLayout);
+
+        binder.readBean(person);
+
+        btSave.addClickListener( e -> {
+            try
+            {
+                binder.writeBean(person);
+                Notification.show(person.toString(), 5000 ,Notification.Position.TOP_CENTER);
+            } catch (ValidationException ex)
+            {
+                Notification.show("Save not successful", 5000 ,Notification.Position.TOP_CENTER);
+            }
+        });
     }
 }
